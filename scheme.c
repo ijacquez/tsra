6ed0949bb151701062cf2239ecee0939e867e4ea
iZarif <aydar.js@gmail.com>
2020-07-01 19:24:22 +0400

    rename scheme_set_output_port_string fuction to ts_set_out_port_str

--- scheme.c
+++ scheme.c
@@ -4875,7 +4875,7 @@ void scheme_set_output_port_file(scheme *sc, FILE *fout) {
   sc->outport=port_from_file(sc,fout,port_output);
 }
 
-void scheme_set_output_port_string(scheme *sc, char *start, char *past_the_end) {
+void ts_set_out_port_str(scheme *sc, char *start, char *past_the_end) {
   sc->outport=port_from_string(sc,start,past_the_end,port_output);
 }
 
