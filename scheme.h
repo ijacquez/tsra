29b825333b7d38a1212bab7ebcad4931fd15debe
iZarif <aydar.js@gmail.com>
2020-07-01 18:04:50 +0400

    SCHEME_EXPORT some functions

--- scheme.h
+++ scheme.h
@@ -191,19 +191,19 @@ SCHEME_EXPORT pointer scheme_eval(scheme *sc, pointer obj);
 void scheme_set_external_data(scheme *sc, void *p);
 SCHEME_EXPORT void scheme_define(scheme *sc, pointer env, pointer symbol, pointer value);
 
-pointer _cons(scheme *sc, pointer a, pointer b, int immutable);
-pointer mk_integer(scheme *sc, long num);
-pointer mk_real(scheme *sc, double num);
-pointer mk_symbol(scheme *sc, const char *name);
-pointer gensym(scheme *sc);
-pointer mk_string(scheme *sc, const char *str);
-pointer mk_counted_string(scheme *sc, const char *str, int len);
-pointer mk_empty_string(scheme *sc, int len, char fill);
-pointer mk_character(scheme *sc, int c);
-pointer mk_foreign_func(scheme *sc, foreign_func f);
-void putstr(scheme *sc, const char *s);
-int list_length(scheme *sc, pointer a);
-int eqv(pointer a, pointer b);
+SCHEME_EXPORT pointer _cons(scheme *sc, pointer a, pointer b, int immutable);
+SCHEME_EXPORT pointer mk_integer(scheme *sc, long num);
+SCHEME_EXPORT pointer mk_real(scheme *sc, double num);
+SCHEME_EXPORT pointer mk_symbol(scheme *sc, const char *name);
+SCHEME_EXPORT pointer gensym(scheme *sc);
+SCHEME_EXPORT pointer mk_string(scheme *sc, const char *str);
+SCHEME_EXPORT pointer mk_counted_string(scheme *sc, const char *str, int len);
+SCHEME_EXPORT pointer mk_empty_string(scheme *sc, int len, char fill);
+SCHEME_EXPORT pointer mk_character(scheme *sc, int c);
+SCHEME_EXPORT pointer mk_foreign_func(scheme *sc, foreign_func f);
+SCHEME_EXPORT void putstr(scheme *sc, const char *s);
+SCHEME_EXPORT int list_length(scheme *sc, pointer a);
+SCHEME_EXPORT int eqv(pointer a, pointer b);
 
 struct scheme_interface {
   void (*scheme_define)(scheme *sc, pointer env, pointer symbol, pointer value);
@@ -275,7 +275,7 @@ typedef struct scheme_registerable
 }
 scheme_registerable;
 
-void scheme_register_foreign_func_list(scheme * sc,
+SCHEME_EXPORT void scheme_register_foreign_func_list(scheme * sc,
                                        scheme_registerable * list,
                                        int n);
 #endif
